b875bfeeb6fdb5989195eb20574c70b95a6b0c57
Jan David <jdno@jdno.dev>
2023-04-12 00:34:28 +0200

    Create module for workflows
    A new module has been created for workflows. The module contains the
    `Workflow` struct, which represents a GitHub Actions workflow, and a
    `WorkflowBuilder`, which can be used to construct workflows. Both are
    empty, since no fields have been added to the workflow yet.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,22 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+//! # Rust Struct for GitHub Actions Workflows
+//!
+//! [GitHub Actions](https://docs.github.com/en/actions) is a continuous integration and continuous
+//! delivery (CI/CD) platform. Users can create workflows that build, test, and deploy code:
+//!
+//! > A workflow is a configurable automated process that will run one or more jobs. Workflows are
+//! > defined by a YAML file checked in to your repository and will run when triggered by an event
+//! > in your repository, or they can be triggered manually, or at a defined schedule.
+//! > -- [GitHub Actions Documentation](https://docs.github.com/en/actions/using-workflows/about-workflows)
+//!
+//! This crate implements a representation of the [workflow syntax] that can be used to read and
+//! write the workflow files.
+//!
+//! [workflow syntax]: https://docs.github.com/en/actions/reference/workflow-syntax-for-github-actions
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// All public items in the crate are documented using the following guidelines:
+// https://rust-lang.github.io/api-guidelines/documentation.html
+#![deny(missing_docs)]
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use self::workflow::*;
+
+mod workflow;
